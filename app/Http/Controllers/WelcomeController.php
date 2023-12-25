17eb69136704f6f54dc397a4f36a4f21d6e73c49
unknown <HichamRB>
2023-12-25 16:14:52 +0100

    generate pdf on welcome page

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Barryvdh\DomPDF\PDF;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\View; // Import the View facade
+
+class WelcomeController extends Controller
+{
+    public function welcomePDF()
+    {
+        $data = [];
+
+        for ($i = 1; $i <= 50; $i++) {
+            $data[] = [
+                'id' => ($i),
+                'name' => 'Product ' . $i,
+                'price' => rand(10, 100),
+            ];
+        }
+
+        $pdf = App::make('dompdf.wrapper');
+        $pdf->loadHTML(View::make('welcome', compact('data')));
+
+        return $pdf->stream('welcome.pdf');
+    }
+}
